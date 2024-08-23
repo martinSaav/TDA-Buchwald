a230cd13d18455d3489bb214cf0b94996ef728c4
Martín Alejandro Estrada Saavedra <martin996@hotmail.com.ar>
2024-08-23 13:45:46 -0300

    carpeta de divsion y conquista y modificaciones en lenguaje de ejercicio 1

--- D&C/ejercicio1.py
+++ D&C/ejercicio1.py
@@ -1,13 +1,11 @@
 def _find_out_of_place(arr, low, high):
-    # Caso base: si solo hay un elemento o dos en la sublista
     if low == high:
         return 0
     
-    # Encuentra el punto medio
     mid = (low + high) // 2
 
-    izq = _find_out_of_place(arr, low, mid)
-    der = _find_out_of_place(arr, mid + 1, high)
+    left = _find_out_of_place(arr, low, mid)
+    rigth = _find_out_of_place(arr, mid + 1, high)
 
     #print(arr[low], ">", arr[mid+1], "and", arr[mid], ">",arr[mid+1], "and", arr[high], ">", arr[mid+1])
     #low izq inicio
@@ -16,15 +14,14 @@ def _find_out_of_place(arr, low, high):
     #high der final
     # izq_inicio > der_inicio y izq_final > der_inicio y der_final > izq_final
     if (arr[low] > arr[mid+1] and arr[mid] > arr[mid+1] and arr[high] > arr[mid]):
-        return izq + der + arr[mid+1]
+        return left + rigth + arr[mid+1]
     if (arr[low] > arr[mid+1] and arr[mid] > arr[mid+1] and arr[high] < arr[mid]):
-        return izq + der + arr[mid]
+        return left + rigth + arr[mid]
     
     if (arr[mid] > arr[mid+1]):
-        return izq + der + arr[mid]
+        return left + rigth + arr[mid]
 
-
-    return izq + der
+    return left + rigth
 
 def find_out_of_place(arr):
     start = 0
@@ -35,35 +32,35 @@ def find_out_of_place(arr):
         return arr[end]
     return _find_out_of_place(arr, start, end)
 
-# Ejemplo de uso
+
 arr = [17, 19, 7, 20] 
-print(f"Elemento fuera de lugar: 7 vs {find_out_of_place(arr)}")
+print(f"Element out of place: 7 vs {find_out_of_place(arr)}")
 
 arr = [19, 7, 20]
-print(f"Elemento fuera de lugar: 19 vs {find_out_of_place(arr)}")
+print(f"Element out of place: 19 vs {find_out_of_place(arr)}")
 
 arr = [20, 1]
-print(f"Elemento fuera de lugar: 20 vs {find_out_of_place(arr)}")
+print(f"Element out of place: 20 vs {find_out_of_place(arr)}")
 
 arr = [20, 25, 1]
-print(f"Elemento fuera de lugar: 1 vs {find_out_of_place(arr)}")
+print(f"Element out of place: 1 vs {find_out_of_place(arr)}")
 
 arr = [20, 25, 1, 26]
-print(f"Elemento fuera de lugar: 1 vs {find_out_of_place(arr)}")
+print(f"Element out of place: 1 vs {find_out_of_place(arr)}")
 
 arr = [6, 1, 9, 10, 12, 13, 14, 15]
-print(f"Elemento fuera de lugar: 6 vs {find_out_of_place(arr)}")
+print(f"Element out of place: 6 vs {find_out_of_place(arr)}")
 
 arr = [2, 3, 15, 16, 19, 33, 4, 35]
-print(f"Elemento fuera de lugar: 4 vs {find_out_of_place(arr)}")
+print(f"Element out of place: 4 vs {find_out_of_place(arr)}")
 
 arr = [1, 2, 3, 15, 11]
-print(f"Elemento fuera de lugar: 15(sirve igual si da 11 pero bueno) vs {find_out_of_place(arr)}")
+print(f"Element out of place: 15(It's okay if the result is 11) vs {find_out_of_place(arr)}")
 
 arr = [111, 2, 3, 10, 11]
-print(f"Elemento fuera de lugar: 111 vs {find_out_of_place(arr)}")
+print(f"Element out of place: 111 vs {find_out_of_place(arr)}")
 
 
 arr = [1,99, 3, 11]
-print(f"Elemento fuera de lugar: 99 vs {find_out_of_place(arr)}")
+print(f"Element out of place: 99 vs {find_out_of_place(arr)}")
 
